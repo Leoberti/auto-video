eac9c6418ee7120d421501bdaf36245393ad9cca
LeoBerti <leoberti@gmail.com>
2019-05-25 15:12:01 -0300

    adding term and prefix

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ function start() {
 const content = {}
 
 content.searchTerm = askAndReturnSearchTerm()
+content.prefix = askAndReturnPrefix()
 
 function askAndReturnSearchTerm () {
     return readline.question('digite o termo da wikipedia: ')
 }
+
+function askAndReturnPrefix(){
+    const prefixes = ['Quem eh', 'O que eh','A Historia de ']
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma option: ')
+    const selectedPrefixText = prefixes[selectedPrefixIndex]
+    
+    return (selectedPrefixText)
+
+ }  
+   
+
 console.log(content)
 }
 start()
